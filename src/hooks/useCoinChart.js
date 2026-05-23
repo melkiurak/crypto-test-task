c89572fb7d4b9e2b9aa4edd30ce072eaf9cfd00b
Danila <melkiurak3209@gmail.com>
2026-05-23 15:57:15 +0300

    task 4 - Course schedule with auto-refresh

---
+++ src/hooks/useCoinChart.js
@@ -0,0 +1,27 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query"
+
+export const useCoinsChart = (coinId) => {
+    return useQuery({
+        queryKey: ["coinChartKey", coinId],
+        queryFn: async () => {
+            const response = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=7`)
+            if (!response.ok) {
+                throw new Error("Не вдалося завантажити дані графіка");
+            }
+            return response.json()
+        },
+        select: (data) => {
+            return data.prices.map(([timestamp, price]) => ({
+                date: new Date(timestamp).toLocaleString("uk-UA", {
+                    day: "numeric",
+                    month: "short",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                }),
+                price: price,
+            }));
+        },
+        refetchInterval: 15000,
+        placeholderData: (keepPreviousData) => keepPreviousData
+    })
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,12 @@
-import { DollarOutlined, HomeOutlined } from "@ant-design/icons";
+import { BarChartOutlined, DollarOutlined, HomeOutlined } from "@ant-design/icons";
 import Home from "./pages/Home.jsx";
 import { Coins } from "./pages/Coins.jsx";
 import { CoinsPaged } from "./pages/CoinsPaged.jsx";
+import { CoinsChartPage } from "./pages/CoinsChartPage.jsx";
 
 export const routes = [
     { path: "/", label: "Home", icon: <HomeOutlined />, element: <Home /> },
     { path: '/coins', label: 'Coins', icon: <DollarOutlined />, element: <Coins/>},
-    { path: '/coins-paged', label: 'Coins Paged', icon: <DollarOutlined />, element: <CoinsPaged/>}
+    { path: '/coins-paged', label: 'Coins Paged', icon: <DollarOutlined />, element: <CoinsPaged/>},
+    { path: "/chart", label: "Chart", icon: <BarChartOutlined />, element: <CoinsChartPage /> }
 ];
